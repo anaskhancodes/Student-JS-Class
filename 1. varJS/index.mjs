8d417496c2acb180b4c89b44249c468393b4b971
M.Anas Khan <a4anas406@gmail.com>
2023-08-22 20:53:05 +0500

    Class1

---
+++ 1. varJS/index.mjs
@@ -0,0 +1,21 @@
+// alert('hello');
+
+// console.log('Hello world');
+
+let name = "Ali";
+
+console.log('name>>', name)
+
+name = "Anas";
+
+console.log("name 2", name);
+
+let rollNum;
+
+rollNum = "248"
+
+console.log(rollNum);
+
+
+
+
---
+++ 2.VarNUM/index.mjs
@@ -0,0 +1,29 @@
+
+// let Num = 248
+// let textnum = "345"
+
+// console.log(Num)
+// console.log(textnum)
+
+// let NumText = textnum + 20;
+
+// console.log(NumText);
+
+
+
+let num1 = 20;
+let num2 = 30;
+
+// console.log(num1 + num2);
+
+let result = num1 + num2
+
+alert(result);
+
+// !@#$%& nOt Charter
+
+// let alert = ("jaljdsaklj")
+
+
+
+
